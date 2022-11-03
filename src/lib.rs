d5c4d16e88a9cb395c590970af95246fa6913e2a
NotAPenguin <michiel.vandeginste@gmail.com>
2022-11-03 20:13:17 +0100

    window interface trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,54 @@ use std::ffi::{CString};
 use std::str::FromStr;
 use ash::{vk, Entry, Instance};
 use ash::extensions::ext::DebugUtils;
+use ash_window;
+use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle, RawDisplayHandle, RawWindowHandle};
 
 mod util;
 
+/// Used as a dummy window interface in case of a headless context. Calling any of the `raw_xxx_handle()` functions on this will result in a panic.
+pub struct HeadlessWindowInterface;
+
+unsafe impl HasRawWindowHandle for HeadlessWindowInterface {
+    fn raw_window_handle(&self) -> RawWindowHandle {
+        panic!("Called raw_window_handle() on headless window context.");
+    }
+}
+
+unsafe impl HasRawDisplayHandle for HeadlessWindowInterface {
+    fn raw_display_handle(&self) -> RawDisplayHandle {
+        panic!("Called raw_display_handle() on headless window context.");
+    }
+}
+
+/// Parent trait combining all requirements for a window interface. To be a window interface, a type T must implement the following traits:
+/// - [`HasRawWindowHandle`](raw_window_handle::HasRawWindowHandle)
+/// - [`HasRawDisplayHandle`](raw_window_handle::HasRawDisplayHandle)
+pub trait WindowInterface: HasRawWindowHandle + HasRawDisplayHandle {}
+impl<T: HasRawWindowHandle + HasRawDisplayHandle> WindowInterface for T {}
+
 /// Application settings used to initialize the phobos context.
-pub struct AppSettings {
+pub struct AppSettings<'a, Window> where Window: WindowInterface {
     /// Application name. Possibly displayed in debugging tools, task manager, etc.
     pub name: String,
     /// Application version.
     pub version: (u32, u32, u32),
     /// Enable Vulkan validation layers for additional debug output. For developing this should almost always be on.
-    pub enable_validation: bool
+    pub enable_validation: bool,
+    /// Optionally a reference to an object implementing a windowing system. If this is not None, it will be used to create a [`VkSurfaceKHR`](vk::SurfaceKHR) to present to.
+    pub window: Option<&'a Window>,
+}
+
+/// Contains all information about a [`VkSurfaceKHR`](vk::SurfaceKHR)
+pub struct Surface {
+    /// Handle to the [`VkSurfaceKHR`](vk::SurfaceKHR)
+    pub handle: vk::SurfaceKHR,
+    /// [`VkSurfaceCapabilitiesKHR`](vk::SurfaceCapabilitiesKHR) structure storing information about surface capabilities.
+    pub capabilities: vk::SurfaceCapabilitiesKHR,
+    /// List of [`VkSurfaceFormatKHR`](vk::SurfaceFormatKHR) with all formats this surface supports.
+    pub formats: Vec<vk::SurfaceFormatKHR>,
+    /// List of [`VkPresentModeKHR`](vk::PresentModeKHR) with all present modes this surface supports.
+    pub present_modes: Vec<vk::PresentModeKHR>
 }
 
 /// Main phobos context. This stores all global Vulkan state. Interaction with the device all happens through this
@@ -24,8 +61,10 @@ pub struct Context {
     vk_entry: Entry,
     /// Stores the handle to the created [`VkInstance`](ash::Instance).
     instance: Instance,
-    /// handle to the [`VkDebugUtilsMessengerEXT`](vk::DebugUtilsMessengerEXT) object. Null if `AppSettings::enable_validation` was `false` on initialization.
-    debug_messenger: vk::DebugUtilsMessengerEXT,
+    /// handle to the [`VkDebugUtilsMessengerEXT`](vk::DebugUtilsMessengerEXT) object. None if `AppSettings::enable_validation` was `false` on initialization.
+    debug_messenger: Option<vk::DebugUtilsMessengerEXT>,
+    /// Surface information and handle. None if `AppSettings::create_headless` was `true` on initialization.
+    surface: Option<Surface>,
 }
 
 extern "system" fn vk_debug_callback(
@@ -50,7 +89,7 @@ extern "system" fn vk_debug_callback(
     false as vk::Bool32
 }
 
-fn create_vk_instance(entry: &Entry, settings: &AppSettings) -> Option<ash::Instance> {
+fn create_vk_instance<Window>(entry: &Entry, settings: &AppSettings<Window>) -> Option<ash::Instance> where Window: WindowInterface {
     let app_name = CString::new(settings.name.clone()).unwrap();
     let engine_name = CString::new("Phobos").unwrap();
     let app_info = vk::ApplicationInfo {
@@ -95,20 +134,32 @@ fn create_debug_messenger(entry: &Entry, instance: &Instance) -> vk::DebugUtilsM
     unsafe { loader.create_debug_utils_messenger(&create_info, None).unwrap() }
 }
 
+fn create_surface<Window>(settings: &AppSettings<Window>, entry: &Entry, instance: &Instance) -> Surface where Window: WindowInterface {
+    let window = settings.window.unwrap();
+    let surface_handle = unsafe { ash_window::create_surface(&entry, &instance, window.raw_display_handle(), window.raw_window_handle(), None).unwrap() };
+    todo!()
+}
+
 impl Context {
-    pub fn new(settings: AppSettings) -> Option<Context> {
+    pub fn new<Window>(settings: AppSettings<Window>) -> Option<Context> where Window: WindowInterface {
         let entry = unsafe { Entry::load().unwrap() };
         let instance = create_vk_instance(&entry, &settings).unwrap();
         let debug_messenger = if settings.enable_validation {
-            create_debug_messenger(&entry, &instance)
+            Some(create_debug_messenger(&entry, &instance))
         } else {
-            vk::DebugUtilsMessengerEXT::null()
+            None
+        };
+        let surface = if settings.window.is_some() {
+            Some(create_surface(&settings, &entry, &instance))
+        } else {
+            None
         };
 
         Some(Context {
             vk_entry: entry,
             instance,
-            debug_messenger
+            debug_messenger,
+            surface
         })
     }
 }
@@ -116,9 +167,9 @@ impl Context {
 impl Drop for Context {
     fn drop(&mut self) {
         unsafe {
-            if self.debug_messenger != vk::DebugUtilsMessengerEXT::null() {
+            if let Some(debug_messenger) = self.debug_messenger {
                 let debug_utils_loader = DebugUtils::new(&self.vk_entry, &self.instance);
-                debug_utils_loader.destroy_debug_utils_messenger(self.debug_messenger, None);
+                debug_utils_loader.destroy_debug_utils_messenger(debug_messenger, None);
             }
             self.instance.destroy_instance(None);
         }
--- tests/main.rs
+++ tests/main.rs
@@ -2,10 +2,11 @@ use phobos as ph;
 
 #[test]
 fn create_context() {
-    let settings = ph::AppSettings {
+    let settings = ph::AppSettings::<ph::HeadlessWindowInterface> {
         version: (1, 0, 0),
         name: String::from("Phobos test app"),
-        enable_validation: true
+        enable_validation: true,
+        window: None
     };
     let ctx = ph::Context::new(settings).unwrap();
 }
