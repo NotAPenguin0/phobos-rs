c9ac4a74ac3f602482c8a0902f8dcddff1946eb4
NotAPenguin <michiel.vandeginste@gmail.com>
2023-03-06 23:52:00 +0100

    add some missing VkFormats

--- src/util.rs
+++ src/util.rs
@@ -32,6 +32,10 @@ impl ByteSize for vk::Format {
             vk::Format::R32G32_SFLOAT => 2 * size_of::<f32>(),
             vk::Format::R32G32B32_SFLOAT => 3 * size_of::<f32>(),
             vk::Format::R32G32B32A32_SFLOAT => 4 * size_of::<f32>(),
+            vk::Format::R8_UNORM => 1,
+            vk::Format::R8G8_UNORM => 2,
+            vk::Format::R8G8B8_UNORM => 3,
+            vk::Format::R8G8B8A8_UNORM => 4,
             _ => { todo!() }
         }
     }
