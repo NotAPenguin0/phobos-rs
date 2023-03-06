a7d1afd923eaf28b455cd9408116b9fe273edf01
NotAPenguin <michiel.vandeginste@gmail.com>
2023-03-06 18:32:10 +0100

    add cmdbuffer::copy_buffer_to_image

--- src/command_buffer.rs
+++ src/command_buffer.rs
@@ -74,6 +74,7 @@ pub trait GraphicsCmdBuffer : TransferCmdBuffer {
 /// Trait representing a command buffer that supports transfer commands.
 pub trait TransferCmdBuffer {
     fn copy_buffer(self, src: &BufferView, dst: &BufferView) -> Result<Self> where Self: Sized;
+    fn copy_buffer_to_image(self, src: &BufferView, dst: &ImageView) -> Result<Self> where Self: Sized;
 }
 
 /// Trait representing a command buffer that supports compute commands.
@@ -427,7 +428,26 @@ impl<D: TransferSupport + ExecutionDomain> TransferCmdBuffer for IncompleteComma
 
         Ok(self)
     }
-    // Methods for transfer commands
+
+    fn copy_buffer_to_image(self, src: &BufferView, dst: &ImageView) -> Result<Self> where Self: Sized {
+        let copy = vk::BufferImageCopy {
+            buffer_offset: src.offset,
+            buffer_row_length: dst.size.width,
+            buffer_image_height: dst.size.height,
+            image_subresource: vk::ImageSubresourceLayers {
+                aspect_mask: dst.aspect,
+                mip_level: dst.base_level,
+                base_array_layer: dst.base_layer,
+                layer_count: dst.layer_count,
+            },
+            image_offset: Default::default(),
+            image_extent: dst.size,
+        };
+
+        unsafe { self.device.cmd_copy_buffer_to_image(self.handle, src.handle, dst.image, vk::ImageLayout::TRANSFER_DST_OPTIMAL, std::slice::from_ref(&copy)); }
+
+        Ok(self)
+    }
 }
 
 impl<D: ComputeSupport + ExecutionDomain> ComputeCmdBuffer for IncompleteCommandBuffer<'_, D> {
