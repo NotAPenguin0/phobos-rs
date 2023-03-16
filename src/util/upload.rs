1cb8e9ce03750cd30a45a290bdd27727ecf14ff3
NotAPenguin <michiel.vandeginste@gmail.com>
2023-03-16 16:26:02 +0100

    Fix leaking upload buffer

--- src/util/upload.rs
+++ src/util/upload.rs
@@ -5,17 +5,17 @@ use anyhow::Result;
 use ash::vk;
 
 /// Perform a staged upload to a GPU buffer. Returns a future that can be awaited to obtain the resulting buffer.
-pub async fn staged_buffer_upload<T: Copy, A: Allocator>(device: Arc<Device>, mut allocator: A, exec: Arc<ExecutionManager>, data: &[T]) -> Result<Buffer<A>> {
+pub async fn staged_buffer_upload<T: Copy, A: Allocator + 'static>(device: Arc<Device>, mut allocator: A, exec: Arc<ExecutionManager>, data: &[T]) -> Result<Buffer<A>> {
     let staging = Buffer::new(device.clone(), &mut allocator, (data.len() * std::mem::size_of::<T>()) as vk::DeviceSize, vk::BufferUsageFlags::TRANSFER_SRC, MemoryType::CpuToGpu)?;
-    let mut staging = staging.view_full();
-    staging.mapped_slice()?.copy_from_slice(data);
+    let mut staging_view = staging.view_full();
+    staging_view.mapped_slice()?.copy_from_slice(data);
 
     let buffer = Buffer::new_device_local(device.clone(), &mut allocator, staging.size, vk::BufferUsageFlags::TRANSFER_DST | vk::BufferUsageFlags::VERTEX_BUFFER)?;
     let view = buffer.view_full();
 
     let cmd =
         exec.on_domain::<domain::Transfer>(None, None)?
-            .copy_buffer(&staging, &view)?
+            .copy_buffer(&staging_view, &view)?
             .finish()?;
 
     ExecutionManager::submit(exec.clone(), cmd)?
