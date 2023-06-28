57417b9eb9fdce704c2e9b597a71c6d31ed76d86
Danny Le <dannylewastaken@protonmail.com>
2023-06-28 17:22:29 -0400

    Documentation + Fix const access error

--- src/core/traits.rs
+++ src/core/traits.rs
@@ -1,13 +1,35 @@
 //! Defines traits for core
 
 use anyhow::Result;
+use crate::Device;
 
-pub unsafe trait AsRawHandle {
-	unsafe fn handle() -> u64;
+/// Direct access to the object's handle's as_raw representation
+pub unsafe trait AsRaw {
+	/// Get the as_raw u64 value of the handle underlying the object
+	unsafe fn as_raw(&self) -> u64;
 }
 
 /// Represents an abstraction for naming objects in Vulkan for debuggers
-pub trait Nameable {
+pub trait Nameable: AsRaw {
 	/// Change the name of the given object
-	fn set_name(&self, device: &crate::core::device::Device, name: &str) -> Result<()>;
+	const OBJECT_TYPE: crate::vk::ObjectType;
+}
+
+impl Device {
+	/// Set the name of any given compatible object for debugging purposes
+	pub fn set_name<T: Nameable>(&self, object: &T, name: &str) -> Result<()> {
+		let object_name = std::ffi::CString::new(name)?;
+		let name_info = crate::vk::DebugUtilsObjectNameInfoEXT::builder()
+			.object_type(<T as Nameable>::OBJECT_TYPE)
+			.object_handle( unsafe {object.as_raw()} )
+			.object_name(&object_name)
+			.build();
+
+		unsafe {
+			Ok(self.debug_utils()?.set_debug_utils_object_name(
+				self.handle().handle(),
+				&name_info
+			)?)
+		}
+	}
 }
